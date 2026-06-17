d2eb31f568386f458a7463b61802259202f4121c
theethesh <theetheshd@gmail.com>
2026-06-18 00:20:11 +0530

    Initial commit from Create Next App

---
+++ app/login/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div className='flex justify-center items-center bg-gray-400'>
+        <input type='text' placeholder='enter a name'></input>
+        <input type='text' placeholder='enter a email'></input>
+        <button>submit</button>
+        <button>register</button>
+        
+      
+    </div>
+  )
+}
+
+
+export default login
